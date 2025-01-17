989472acdc27d611632f090ce777fed8ce078fac
OursBlanc42 <simon.regnier@holbertonstudents.com>
2025-01-17 10:06:03 +0100

    update calculator (fix return error code)

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,20 +3,22 @@ from calculator_1 import add, sub, mul, div
 import sys
 
 """
-
+This program do basic calculation
+Usage: ./100-my_calculator.py <a> <operator> <b>
 """
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
     operator = ["+", "-", "*", "/"]
     funcall = [add, sub, mul, div]
 
     if sys.argv[2] not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
