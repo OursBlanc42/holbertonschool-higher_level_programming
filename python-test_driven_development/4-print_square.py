e88995d9b2a75e10eec7453ed678c82da31dd66b
OursBlanc42 <regniersimon@hotmail.com>
2025-01-24 16:47:29 +0100

    flake8 linter compliance

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,6 +4,7 @@
 Module for print_square function
 """
 
+
 def print_square(size):
     """
     prints a square with the character #.
@@ -15,11 +16,11 @@ def print_square(size):
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-        
+
     if size <= 0:
         raise ValueError("size must be >= 0")
-        
-    for index in range(0,size):
-        for index in range(0,size):
-            print("#", end = "")
+
+    for index in range(0, size):
+        for index in range(0, size):
+            print("#", end="")
         print("")
