a2a33ad7c5a71c2674a5d1c01436d59c06f92616
OursBlanc42 <regniersimon@hotmail.com>
2025-01-24 12:33:16 +0100

    pycodestyle linter compliance

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 """
-Module for basic calculation (add). 
-    
+Module for basic calculation (add).
 """
+
 
 def add_integer(a, b=98):
     """
@@ -12,21 +12,20 @@ def add_integer(a, b=98):
     Args:
         a (int): first integer
         b (int, optional): second integer. Defaults to 98.
-        
+
 
     """
-    
+
     if type(a) is float:
         a = int(a)
-        
+
     if type(b) is float:
         b = int(b)
-    
+
     if type(a) is not int:
         raise TypeError("a must be an integer")
-    
+
     if type(b) is not int:
         raise TypeError("b must be an integer")
 
     return (a + b)
-
