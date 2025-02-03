77c48b7eba2c5afd760bec70e7b8ab402b504f74
OursBlanc42 <regniersimon@hotmail.com>
2025-02-03 15:26:32 +0100

    modify check for type and add task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -39,3 +39,27 @@ class BaseGeometry():
 
         if value <= 0:
             raise ValueError(f"{self.name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("height", self.__height)
+        self.integer_validator("height", self.__width)
+
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
