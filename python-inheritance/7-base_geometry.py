2dbba9e35e8d1b2be8d04d33c342da82ae71d043
OursBlanc42 <regniersimon@hotmail.com>
2025-02-03 14:34:15 +0100

    add task 6 and 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+    Module for base_geometry
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry Improve base geometry with a public instance method
+    """
+    def area(self):
+        """
+        area pass
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        integer_validator Validate the "value" variable
+
+        Args:
+            name (string): Name of the value (always a string)
+            value (int): Value
+
+        Raises:
+            TypeError: Must be an integer
+            ValueError: Must be >= 0
+        """
+        self.name = name
+
+        if isinstance(value, int) is not True:
+            raise TypeError(f"{self.name} must be an integer")
+        else:
+            self.value = value
+
+        if value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
