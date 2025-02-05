f521d275bdaf69dd05ed64dab8b2b7b18ab36704
OursBlanc42 <regniersimon@hotmail.com>
2025-02-05 11:16:08 +0100

    enhance if loop

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -33,6 +33,7 @@ class BaseGeometry():
 
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
+        elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+        else:
+            pass
