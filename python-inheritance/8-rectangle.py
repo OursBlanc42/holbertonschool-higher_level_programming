7c5280fedfe9ab9092842495e7c89217487a6d71
OursBlanc42 <regniersimon@hotmail.com>
2025-02-05 15:20:29 +0100

    add task 8 with check the true return

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle(BaseGeometry):
         BaseGeometry (_type_): _description_
     """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        if self.integer_validator("width", width) is True:
+            self.__width = width
+        if self.integer_validator("height", height) is True:
+            self.__height = height
