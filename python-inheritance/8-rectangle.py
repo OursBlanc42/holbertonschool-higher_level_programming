25af0ef2636625ff5274ed67d9a0de8443bdd1c4
OursBlanc42 <regniersimon@hotmail.com>
2025-02-05 10:53:31 +0100

    code rewriting to synthetise and propper way

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -30,15 +30,13 @@ class BaseGeometry():
             TypeError: Must be an integer
             ValueError: Must be >= 0
         """
-        self.name = name
 
         if type(value) is not int:
-            raise TypeError(f"{self.name} must be an integer")
-
-        self.value = value
+            raise TypeError(f"{name} must be an integer")
 
         if value <= 0:
-            raise ValueError(f"{self.name} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
+        
 
 
 class Rectangle(BaseGeometry):
@@ -49,7 +47,7 @@ class Rectangle(BaseGeometry):
         BaseGeometry (_type_): _description_
     """
     def __init__(self, width, height):
-        self.integer_validator("height", self.__height)
-        self.integer_validator("width", self.__width)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
