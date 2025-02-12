50a11723f8fbe5664953d46628e956169f9e26fa
OursBlanc42 <regniersimon@hotmail.com>
2025-02-12 16:31:05 +0100

    add task_03

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,29 +1,58 @@
 #!/usr/bin/python3
+"""
+    Serialize and deserialize from XML
+"""
 
 import xml.etree.ElementTree as ET
-import json
 
 
 def serialize_to_xml(dictionary, filename):
     """
     serialize_to_xml write XML in specify file
+    algo :
+    1 - create root element
+    2 - add each element from dict as a child of root
+    3 - save XML file
 
     Args:
         dictionary (dict): data
         filename (string): filename
     """
-    root_element = "<data>"
+    root_element = ET.Element("data")
+
+    for key, value, in dictionary.items():
+        child = ET.SubElement(root_element, key)
+        child.text = str(value)
+
+    tree = ET.ElementTree(root_element)
+    try:
+        with open(filename, "wb") as xml_file:
+            tree.write(xml_file, encoding="utf-8", xml_declaration=False)
+    except IOError:
+        return None
 
 
 def deserialize_from_xml(filename):
+    """
+    deserialize_from_xml
 
-    # create a tree from xmml
-    tree = ET.parse(filename)
+    Args:
+        filename (str): XML file to read
 
-    # catch the root
-    root = tree.getroot()
+    Returns:
+        dict: the deserialized dictionnary
+    """
 
-    my_dict = {}
+    try:
+        tree = ET.parse(filename)  # catch the tree
+        root = tree.getroot()  # catch the root
 
-    for child in root:
-        my_dict[child.tag] = child.text
+        my_dict = {}
+
+        for child in root:
+            my_dict[child.tag] = child.text
+
+        return my_dict
+
+    except (FileNotFoundError, ET.ParseError):
+        return None
