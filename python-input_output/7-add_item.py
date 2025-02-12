a2a5d7b76a2c6afb6a85247cc23025eab8ec6663
OursBlanc42 <regniersimon@hotmail.com>
2025-02-12 15:56:45 +0100

    change var name

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,14 +9,14 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-export_filename = "add_item.json"
+filename = "add_item.json"
 
-if os.path.isfile(export_filename) is True:
-    buffer = load_from_json_file(export_filename)
+if os.path.isfile(filename) is True:
+    buffer = load_from_json_file(filename)
 else:
     buffer = []
 
 for arg in sys.argv[1:]:
     buffer.append(arg)
 
-save_to_json_file(buffer, export_filename)
+save_to_json_file(buffer, filename)
