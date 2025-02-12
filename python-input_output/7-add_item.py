6d98efe67614528a66a52b7c8ad2a3a892fd510c
OursBlanc42 <regniersimon@hotmail.com>
2025-02-12 15:54:57 +0100

    remove try except

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,15 +4,16 @@
 """
 
 import sys
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 export_filename = "add_item.json"
 
-try:
+if os.path.isfile(export_filename) is True:
     buffer = load_from_json_file(export_filename)
-except FileNotFoundError:
+else:
     buffer = []
 
 for arg in sys.argv[1:]:
