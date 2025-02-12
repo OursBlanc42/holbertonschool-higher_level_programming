71fa8097974392a68c8b81e2cead40f92abb97aa
OursBlanc42 <regniersimon@hotmail.com>
2025-02-12 16:35:49 +0100

    fix filename default

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 
+"""
+    reading data from one format (CSV) and converting it into
+    another format (JSON) using serialization techniques.
+"""
+
 import csv
 import json
 
 
 def convert_csv_to_json(filename=None):
+    """
+    convert_csv_to_json
 
-    with open('data.csv', encoding='utf-8', newline='') as csvfile:
+    Args:
+        filename (_type_, optional): _description_. Defaults to None.
+    """
+
+    with open(filename, encoding='utf-8', newline='') as csvfile:
 
         csv_reader = csv.DictReader(csvfile)
 
@@ -15,4 +26,4 @@ def convert_csv_to_json(filename=None):
             data.append(row)
 
     with open("data.json", "w", encoding="utf-8") as jsonfile:
-        jsonfile.write(json.dumps(data))
+        json.dump(data, jsonfile, indent=4)
